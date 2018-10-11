a1d6550dde709a668321ea0965ec88f0ffdd9134
harrysio <waihou.sio@sjsu.edu>
2018-10-11 11:46:49 -0700

    change

---
+++ in-class_exercise_1/sketch.js
@@ -0,0 +1,20 @@
+// declare variables here
+
+
+function setup() {
+  // put setup code here --> this runs once upon launch
+
+  createCanvas(500, 500);
+  background(0);
+
+  // draw ellipse
+ ellipse(100, 100, 80, 80);
+}
+
+
+function draw() {
+  // put drawing code here --> this loops every frame
+}
+
+
+// write custom functions here
